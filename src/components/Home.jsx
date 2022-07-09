f70e5e54451334018a0816253365051bbc16b5f3
Gautam <kumarguptagautam20@gmail.com>
2022-07-09 15:14:37 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Home.js"
 import HeroImage from "../photo/HeroImage.png";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
