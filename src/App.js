63f7874993f5a1fb4ad354e58fbfcdcbdcfc7fec
Gautam <kumarguptagautam20@gmail.com>
2022-07-12 20:51:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
-import Footer from "./components/Footer";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar"; 
 import SocialLinks from "./components/SocialLinks";
@@ -14,21 +13,12 @@ function App() {
   return (
  <div>
   <NavBar />
-
   <Home />
-
-
-  <About />
-
-  <Education/>
-  
-  <Experience/>
-
-<Contact/> 
-
-  {/* <Footer/> */}
-  
   <SocialLinks/>
+  <About />
+  <Education/>
+  <Experience/>
+<Contact/> 
  </div>
   );
 }
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,75 +1,86 @@
-import React from 'react';
-import {FaFacebook,FaYoutube,FaInstagram} from 'react-icons/fa';
+import React from "react";
+import { FaFacebook, FaYoutube, FaInstagram } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
-    const links = [
-        {
-        id:1,
-        child: (
-             <>
-            Facebook <FaFacebook size={30} />
-            </>
-        ), 
-        href:"https://www.facebook.com/profile.php?id=100007330568623",
-        style: "rounded-tr-md"
-    },  
-      {
-        id:3,
-        child: (
-             <>
-            Instagram <FaInstagram size={30} />
-            </>
-        ), 
-        href:"https://www.instagram.com/invites/contact/?i=1an9ks85b6uiw&utm_content=1fn9802",
-        style: "rounded-tr-md"
-    },   
+  const links = [
     {
-        id:2,
-        child: (
-             <>
-            Youtube <FaYoutube size={30} />
-            </>
-        ), 
-        href:"https://youtube.com/channel/UCEjf2d4JOraN893cJjGkS6g",
-        style: "rounded-tr-md"
-    },  
-     {
-        id:4,
-        child: (
-             <>
-            Mail <HiOutlineMail size={30}/>
-            </>
-        ), 
-        href:"mailto:kumarguptagautam20@gmail.com"
+      id: 1,
+      child: (
+        <>
+          Facebook <FaFacebook size={30} />
+        </>
+      ),
+      href: "https://www.facebook.com/profile.php?id=100007330568623",
+      style: "rounded-tr-md",
     },
     {
-        id:1,
-        child: (
-             <>
-            Resume <BsFillPersonLinesFill size={30} />
-            </>
-        ), 
-        href:"/resume.pdf",
-        style: "rounded-br-md",
-        download: true
+      id: 3,
+      child: (
+        <>
+          Instagram <FaInstagram size={30} />
+        </>
+      ),
+      href: "https://www.instagram.com/invites/contact/?i=1an9ks85b6uiw&utm_content=1fn9802",
+      style: "rounded-tr-md",
     },
-]
-  return ( 
-    <div className=' hidden  lg:flex  flex-col top-[50%] left-0 fixed'>
-<ul>
-    {links.map(({id,child,href,style,download}) => (
-    <li key={id}  className={'flex justify-between items-center w-40 h-10 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-800 ' + " " + style}>
-        <a href={href} className="flex justify-between items-center w-full text-white" download={download}
-            target="_blank rel=noreferrer">
-        {child }
-        </a>
-        </li>
-         ))}
-     </ul>
+    {
+      id: 2,
+      child: (
+        <>
+          Youtube <FaYoutube size={30} />
+        </>
+      ),
+      href: "https://youtube.com/channel/UCEjf2d4JOraN893cJjGkS6g",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:kumarguptagautam20@gmail.com",
+    },
+    {
+      id: 5,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+  return (
+    <div className=" hidden lg:flex flex-col top-[30%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-10 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-800" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank" rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 };
 
-export default SocialLinks
+export default SocialLinks;
